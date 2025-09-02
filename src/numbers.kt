72454d7793c1032a67792f5d2510232d4b88453c
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2025-09-02 15:20:31 -0300

    operators

--- src/numbers.kt
+++ src/numbers.kt
@@ -1,8 +1,17 @@
+//fun main(){
+//    println("Por favor, ingrese un número: ")
+//    var numeroIngresado = readln()?:""
+//    var numeroConvertido = numeroIngresado.toInt()
+//    var doubleVariable = 123456789123456
+//    var multiplicacion = numeroConvertido * doubleVariable
+//    println("El número ingresado fue: ${numeroConvertido}, y multiplicado por el long es: ${multiplicacion}")
+//    println("El tipo de número es: ${multiplicacion::class.java}")
+//}
+
 fun main(){
-    println("Por favor, ingrese un número: ")
-    var numeroIngresado = readln().toInt()
-    var doubleVariable = 123456789123456
-    var multiplicacion = numeroIngresado * doubleVariable
-    println("El número ingresado fue: ${numeroIngresado}, y multiplicado por el long es: ${multiplicacion}")
-    println("El tipo de número es: ${multiplicacion::class.java}")
+    println("Ingrese la cant de dinero de su cuenta: ")
+    var dineroIngresado = readln()
+    var dinero = dineroIngresado.toInt()
+    var dineroTriplicado = dinero * 3
+    println("La cantidad total de dinero ganado en el concurso es ${dineroTriplicado}")
 }
