260c08b2dd85d583f04f6bec8ca8d5429912ccfc
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2025-09-05 10:26:14 -0300

    exception tast

--- src/exceptions.kt
+++ src/exceptions.kt
@@ -1,8 +1,35 @@
+//fun main(){
+//    println("Ingrese un número: ")
+//    var test = readln()
+//    try{
+//        println(test?.toInt())
+//    } catch (error: Exception){
+//        error.printStackTrace()
+//        println("Ha ocurrido un error: ${error} \n${error.localizedMessage}")
+//    } finally {
+//        println("Se terminó la ejecución")
+//    }
+//}
+
+//fun main(){
+//    println("Ingrese un número: ")
+//    var input = readln()
+//    try{
+//        println(input?.toInt())
+//    } catch (error: Exception){
+//        throw IllegalStateException("No me gustó este input, perro: ${error}")
+//    }
+//}
+
 fun main(){
-    var test = readln()
-    try{
-        println(test?.toInt())
+    println("Ingresa un número: ")
+    var input = readln()
+    try {
+        var numero = input?.toInt()
+        var numeroPorCinco = numero?.times(5)
+        println("El número ingresdo fue ${input}, multiplicado por 5 es = ${numeroPorCinco}")
     } catch (error: Exception){
-        println("Ha ocurrido un error: ${error}")
+        println("Ha ocurrido un error: ${error} " +
+                "${error.localizedMessage}")
     }
 }
